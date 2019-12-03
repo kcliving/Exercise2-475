1591db1cfc5bce8cb38a4f5c8cbe0d99ffc4993a
atenudel <aten@udel.edu>
2019-12-03 09:30:07 -0500

    working home page

--- Exercise2/src/app/register/register.component.ts
+++ Exercise2/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-register',
+  selector: 'app-personal',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
